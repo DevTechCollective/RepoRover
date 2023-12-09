172676758e1bcf7f93d0df09635e06556385c41b
Marcozc19 <zhuangc19@gmail.com>
2023-12-09 18:58:14 -0500

    completed app and streamlit.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ class RunQuery(object):
     """
     This is a class that runs a query.
     """
-    def __init__(self, config):
+    def __init__(self):
         self.config = AIConfigRuntime.load('aiconfig.json')
         self.param = ""
 
@@ -12,7 +12,7 @@ class RunQuery(object):
         """
         This is a method that runs a query.
         """
-        self.param["user_input"] = input
+        self.param = {"user_input": input}
         
         inference_options = InferenceOptions(stream=True) # Defines a console streaming callback
         result = await self.config.run("cell 4", self.param, options=inference_options)
@@ -22,6 +22,7 @@ class RunQuery(object):
         """
         This is a method that passes in the url
         """
-        self.param["url"] = url
+        self.param = {"url": url}
         await self.config.run("cell 3", self.param)
+        
         
--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import threading
+import asyncio
+from app import RunQuery
+
+config = RunQuery()
 
 # Title for your app
 st.title("RepoRover")
@@ -8,5 +13,41 @@ user_input = st.text_input("Enter a Repo URL")
 
 # Button
 if st.button("Learn the Repo"):
+    if user_input:
+        thread = threading.Thread(target=config.update_url(user_input))
+        thread.start()
+
+        # Show a message while the thread is running
+        st.info("Processing... Please wait.")
+
+        # Join the thread to ensure it completes before moving on
+        thread.join()
+        st.success("Done!")
+
+        #generate chatbot area
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        if prompt := st.chat_input("Ask me anything about this repo"):
+            st.chat_message("user").markdown(prompt)
+            st.session_state.messages.append({"role": "user", "content": prompt})
+
+            chat_response = asyncio.run(config.get_query(prompt))
+
+            response = f"AI: {chat_response}"
+
+            with st.chat_message("assistant"):
+                st.markdown(response)
+
+            st.session_state.messages.append({"role": "assistant", "content": response})
+
+    else: 
+        st.write("Please enter a URL")
     # What to do after button is clicked
+
+
     
